b8483fbf7857d197c937bfba60243461446f81d8
Vítor Bárrios Luís de Albuquerque <vbluis.play@gmail.com>
2026-01-06 17:34:07 -0300

    Initial Commit

---
+++ ServerDrivenUI with MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServerDrivenUI with MVVM-C
+//
+//  Created by Vítor Bárrios Luís de Albuquerque on 06/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
