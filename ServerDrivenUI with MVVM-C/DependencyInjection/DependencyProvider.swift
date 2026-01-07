b7f0c97d97b59c064983feae4b6ea9acafcd95a2
Vítor Bárrios Luís de Albuquerque <vbluis.play@gmail.com>
2026-01-07 09:53:08 -0300

    Cria classe DependencyProvider

---
+++ ServerDrivenUI with MVVM-C/DependencyInjection/DependencyProvider.swift
@@ -0,0 +1,23 @@
+//
+//  DependencyProvider.swift
+//  ServerDrivenUI with MVVM-C
+//
+//  Created by Vítor Bárrios Luís de Albuquerque on 07/01/26.
+//
+
+import Foundation
+import Swinject
+
+public class DependencyProvider {
+    public let container = Container()
+    public let assembler: Assembler
+    
+    public init() {
+        assembler = Assembler([],
+                              container: container)
+    }
+    
+    public func add(_ assemblies: [Assembly]) {
+        assembler.apply(assemblies: assemblies)
+    }
+}
