a0e873b939671ff3c5ccf60df9ffc3d447be1f4c
Vítor Bárrios Luís de Albuquerque <vbluis.play@gmail.com>
2026-01-08 13:49:30 -0300

    Cria extensão de UIView para facilitar uso de constraints

--- ServerDrivenUI with MVVM-C/Extensions/UIView+Extensions.swift
+++ ServerDrivenUI with MVVM-C/Extensions/UIView+Extensions.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView {
+    public func constraintToSuperView(top: CGFloat = 0,
+                                      leading: CGFloat = 0,
+                                      trailing: CGFloat = 0,
+                                      bottom: CGFloat = 0) {
+        guard let superview else { return }
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor, constant: top),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: leading),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: trailing),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: bottom)
+        ])
+    }
+}
