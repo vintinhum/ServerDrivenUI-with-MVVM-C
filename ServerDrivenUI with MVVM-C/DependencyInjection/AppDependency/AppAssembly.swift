be0b3fc4516b4c9a8286698ffddaaa84ae76d1f1
Vítor Bárrios Luís de Albuquerque <vbluis.play@gmail.com>
2026-01-09 18:35:51 -0300

    Registra Factory, Coordinator e Service na AppAssembly

--- ServerDrivenUI with MVVM-C/DependencyInjection/AppDependency/AppAssembly.swift
+++ ServerDrivenUI with MVVM-C/DependencyInjection/AppDependency/AppAssembly.swift
@@ -5,9 +5,30 @@
 //  Created by Vítor Bárrios Luís de Albuquerque on 07/01/26.
 //
 
-import Foundation
 import Swinject
+import UIKit
 
 class AppAssembly: Assembly {
-    func assemble(container: Container) { }
+    func assemble(container: Container) {
+        
+        // MARK: - FACTORY
+        
+        container.register(FactoryProtocol.self) { resolver in
+            return Factory(resolver: resolver)
+        }
+        
+        // MARK: - COORDINATOR
+        
+        container.register(CoordinatorProtocol.self) { (resolver, navigationController: UINavigationController) in
+            let factory = resolver.resolveUnwrapping(FactoryProtocol.self)
+            return Coordinator(navigationController: navigationController,
+                               factory: factory)
+        }
+        
+        // MARK: - SERVICE
+        
+        container.register(ServiceProtocol.self) { _ in
+            return Service()
+        }
+    }
 }
