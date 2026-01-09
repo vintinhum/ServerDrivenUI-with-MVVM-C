3d185414460a9c2cd5d4ea54433324904d6073c7
Vítor Bárrios Luís de Albuquerque <vbluis.play@gmail.com>
2026-01-09 18:36:48 -0300

    Configura Root View Controller

--- ServerDrivenUI with MVVM-C/SceneDelegate.swift
+++ ServerDrivenUI with MVVM-C/SceneDelegate.swift
@@ -6,17 +6,27 @@
 //
 
 import UIKit
+import Swinject
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var container: Container?
+    var coordinator: CoordinatorProtocol?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let dependencyProvider = DependencyProvider()
+        container = dependencyProvider.container
+        
+        let navigationController = UINavigationController()
+        coordinator = container?.resolveUnwrapping(CoordinatorProtocol.self, argument: navigationController)
+        coordinator?.start()
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
