e98143b21577ada437586135ba0ab5b9e803c85d
Vítor Bárrios Luís de Albuquerque <vbluis.play@gmail.com>
2026-01-15 11:23:45 -0300

    Registra Use Case da Home na AppAssembly

--- ServerDrivenUI with MVVM-C/DependencyInjection/AppDependency/AppAssembly.swift
+++ ServerDrivenUI with MVVM-C/DependencyInjection/AppDependency/AppAssembly.swift
@@ -10,29 +10,42 @@ import UIKit
 
 class AppAssembly: Assembly {
     func assemble(container: Container) {
+        factoryRegisters(container)
+        coordinatorRegisters(container)
+        serviceRegisters(container)
         
-        // MARK: - FACTORY
-        
+        retrieveHomeUseCaseRegisters(container)
+    }
+    
+    // MARK: - FACTORY
+    
+    private func factoryRegisters(_ container: Container) {
         container.register(FactoryProtocol.self) { resolver in
             return Factory(resolver: resolver)
         }
-        
-        // MARK: - COORDINATOR
-        
+    }
+    
+    // MARK: - COORDINATOR
+    
+    private func coordinatorRegisters(_ container: Container) {
         container.register(CoordinatorProtocol.self) { (resolver, navigationController: UINavigationController) in
             let factory = resolver.resolveUnwrapping(FactoryProtocol.self)
             return Coordinator(navigationController: navigationController,
                                factory: factory)
         }
-        
-        // MARK: - SERVICE
-        
+    }
+    
+    // MARK: - SERVICE
+    
+    private func serviceRegisters(_ container: Container) {
         container.register(ServiceProtocol.self) { _ in
             return Service()
         }
-        
-        // MARK: - USE CASES
-        
+    }
+    
+    // MARK: - USE CASES
+    
+    private func retrieveHomeUseCaseRegisters(_ container: Container) {
         container.register(RetrieveHomeUseCaseProtocol.self) { resolver in
             let service = resolver.resolveUnwrapping(ServiceProtocol.self)
             let sectionFactory = resolver.resolveUnwrapping(ComponentSectionFactoryProtocol.self)
